94b51995bfc9f25680841baa78c8deb3c4806521
Yuriy Ankudinov <stremlenye@gmail.com>
2015-02-02 18:11:00 +0300

    Added TODO to copmponent

--- core/components/index.jsx
+++ core/components/index.jsx
@@ -1,13 +1,16 @@
+/**
+ * Base element, which will display active module on demand
+ */
+
 var React = require('react');
 
 var App = React.createClass({
-
+  //TODO implement display active module display logic
   render: function() {
     return (
-      <div />
+      null
     );
   }
-
 });
 
 module.exports = App;
