3862924c000cbe9216175ba204642f37139b227f
Yuriy Ankudinov <stremlenye@gmail.com>
2015-02-06 02:34:54 +0300

    fixed package.json for complex example

--- examples/complex/app/modules/index/index.jsx
+++ examples/complex/app/modules/index/index.jsx
@@ -16,16 +16,23 @@ var Index = React.createClass({
     this.activateModule(name);
   },
 
+  test: function () {
+    console.log("clicked");
+  },
+
   render: function() {
     return (
       <div>
-        <ul>
-          <li onClick={this.loadModule.bind(this,'dashboard')}>Dashboard</li>
-          <li onClick={this.loadModule.bind(this,'products')}>Products</li>
-        </ul>
+        <button onClick={this.test}>Dashboard</button>
+        <button onClick={this.loadModule.bind(this,'products')}>Products</button>
       </div>
     );
   }
 });
 
 module.exports = Index;
+
+/*
+<li onClick={this.loadModule.bind(this,'dashboard')}>Dashboard</li>
+<li onClick={this.loadModule.bind(this,'products')}>Products</li>
+*/
